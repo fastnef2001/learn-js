30ceea580d24974f30abe587e115265be1fa6134
fastnef2001 <nhanhtqgcd191169@fpt.edu.vn>
2022-05-25 13:38:32 +0700

    update code array

--- js/array.js
+++ js/array.js
@@ -38,6 +38,113 @@ console.log("----------7.unshift thêm phần tử vào đầu mảng-----------
 students2.unshift("Khoa");
 console.log(students2);
 
+console.log("----------8.pop xóa phần tử cuối cùng------------")
+students2.pop();
+console.log(students2);
 
-//Phương thức hay dùng trong mãng
+console.log("----------9.shift xóa phần tử đầu tiên trong mảng------------")
+students2.shift();
+console.log(students2);
 
+console.log("----------10.slice------------")
+
+console.log("----------10.1 slice tạo ra mảng mới copy từ mảng ban đầu------------")
+const animals = ["tiger", "Lion", "horse", "elephant"]
+const animals1 = animals.slice();
+console.log(animals1);
+
+console.log("----------10.2 slice(start) start là vị trí index ở trong mảng------------")
+const animals2 = animals.slice(1);
+console.log(animals2);
+
+console.log("----------10.3 slice(start, end) start là vị trí index bắt đầu, end là vị trí index kết thúc------------")
+const animals3 = animals.slice(1, 3);
+console.log(animals3);
+const animals4 = animals.slice(-3);
+console.log(animals4);
+
+
+
+console.log("----------11.splice nó sẽ xóa phần tử trong mảng hoặc thay thế------------")
+const pets = ["dog", "cat", "bird", "dragon"];
+
+// splice(start)
+console.log("----------11.1 splice(start)------------")
+const pets1 = pets.splice(2);
+console.log(pets1);
+
+// splice(start, deleteCount) : deleteCount là số lượng phần tử muốn xóa hoặc thay thế
+console.log("----------11.2 splice(start, deleteCount) : deleteCount là số lượng phần tử muốn xóa hoặc thay thế------------")
+const petscopy = ["dog", "cat", "bird", "dragon"];
+const pets2 = petscopy.splice(0,2);
+console.log(petscopy);
+
+console.log("----------11.3 splice(start, deleteCount, item1, item2, itemN) deleteCount là số lượng phần tử muốn xóa hoặc thay thế------------")
+const petscopy1 = ["dog", "cat", "bird", "dragon"];
+const pets3 = petscopy1.splice(0,2,"Nhanh", "Hung");
+console.log(petscopy1);
+
+
+console.log("----------12.sort------------")
+
+console.log("----------12.1 sắp xếp các phần tử trong mảng theo chuẩn unicode-16 ------------")
+const random = [1, 9999, 10, 5, 09];
+console.log(random.sort());
+//sort function(a, b))
+//function(callback)
+// const random1 = random.sort(function(a, b) {
+//     if (a > b) return 1; // sắp xếp theo tăng dần
+//                return -1;// sắp xếp theo theo giảm dần
+// })
+const random2 = random.sort((a,b) => (a>b ? 1 : -1));
+console.log(random2);
+
+console.log("----------13.find trả về phần tử tìm thấy đầu tiên trong mảng thỏa điều kiện nào đó------------")
+const numbers = [1, 9999, 10000000, 5, 09];
+// tìm phần tử đầu tiên trong mảng lớn hơn 10
+const findYourNumber = numbers.find((element) => element > 10);
+console.log(findYourNumber);
+//nếu không tìm thấy thì nó sẽ trả về undefined
+
+console.log("----------14.findIndex nó trả về index đầu tiên trong mảng thỏa điều kiện nòa đó------------")
+const findYourIndex = numbers.findIndex((element)=>element>10);
+console.log(findYourIndex);
+//nếu không tìm thấy thì sẽ trả ra kết quả là -1
+
+console.log("----------15.map duyệt qua từng element trong mảng và trả ra một mảng mới mà không thay đổi mảng ban đầu------------")
+const listNumber = [1, 2, 3, 4, 5];
+
+console.log("----------15.1 trả ra một mảng mới mà các số trong mảng cũ nhân 2------------")
+// const listNumberDouble = listNumber.map(function (value, index, array) {
+// return value * 2;
+// });
+
+const listNumberDouble = listNumber.map((value, index, array) => value *2);
+console.log(listNumberDouble);
+
+console.log("----------16.forEach ------------")
+const listNumberTripple = listNumber.forEach((value, index, array) => {
+
+});
+// forEach không thể dừng lại tại vì nó không thể return như map
+// map trả ra mảng mới dựa vào mảng ban đầu còn forEach thì không
+
+console.log("----------17.filter sàn lọc các phần tử trong mảng thỏa điều kiện nào đó ------------")
+const greaterThan3 = listNumber.filter((value, index, array) => value > 3);
+console.log(greaterThan3);
+
+console.log("----------18.some------------")
+// trả về true khi thỏa 1 điều kiện và trả về false khi không thỏa điều kiện nào cả
+const someNumber = listNumber.some((value, index, array) => value > 10);
+console.log(someNumber);
+
+
+console.log("----------19.every------------")
+// mọi điều kiện điều phải đúng thì mới trả về True và ngược lại
+const someNumber1 = listNumber.every((value, index, array) => value > 0);
+console.log(someNumber1);
+
+console.log("----------20.reduce------------")
+// gom các phần tử trong mảng lại thành 1
+const totalNumber = listNumber.reduce((previosValue, currentValue) => previosValue + currentValue, 0);
+console.log(totalNumber);
